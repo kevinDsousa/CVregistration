b4d56cf09ff21d98f62d0e40a8b45bb00528dc36
kevindsousa <kevindiegodasilvasousa@gmail.com>
2023-09-23 19:27:33 -0300

    fix: Reconfigurando o front com storybook

--- app/frontend/src/app/app.module.ts
+++ app/frontend/src/app/app.module.ts
@@ -3,26 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './layout/header/header.component';
-
-//material
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MenuComponent } from './components/menu/menu.component';
-import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    MenuComponent,
-    FooterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
