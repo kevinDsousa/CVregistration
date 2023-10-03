af5c9b764deda27b9588509e08ff8a7e310648aa
kevindsousa <kevindiegodasilvasousa@gmail.com>
2023-10-03 08:37:13 -0300

    feat: testado insersão de valores no backend

--- app/backend/src/app.module.ts
+++ app/backend/src/app.module.ts
@@ -5,10 +5,22 @@ import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { CurriculoModule } from './curriculo/curriculo.module';
 import { SkillsModule } from './skills/skills.module';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule, CurriculoModule, SkillsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    UserModule,
+    CurriculoModule,
+    SkillsModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET_KEY,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, JwtModule],
 })
 export class AppModule {}
